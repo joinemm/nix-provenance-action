bc71ebea8ede6de560b78a6a1618d04d606dfac4
Joonas Rautiola <joonas.rautiola@unikie.com>
2024-01-17 11:24:42 +0200

    remove old stuff

--- default.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {}; stdenv.mkDerivation {
-    pname = "nix-hello";
-    version = "0.0.1";
-    src = ./src;
-    installPhase = ''
-        mkdir -p $out/bin
-        mv hello $out/bin/hello
-    '';
-}
