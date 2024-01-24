d79ad446838ccedfd506e8a96287e34c824dee56
Joonas Rautiola <joonas.rautiola@unikie.com>
2024-01-24 22:31:08 +0200

    cleanup

--- src/nix-build-with-provenance.sh
+++ src/nix-build-with-provenance.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-set -x
 set -e
 
 PROVENANCE_TIMESTAMP_BEGIN="$(date +%s)"
+
 echo "--- Starting nix build ---"
 
 nix build "${@}"
-PROVENANCE_EXTERNAL_PARAMETERS="$(jq -n --arg target "$1" --arg build-args "${2:@}" '$ARGS.named')"
 
+PROVENANCE_EXTERNAL_PARAMETERS="$(jq -n --arg target "$1" --arg build-args "${2:@}" '$ARGS.named')"
 PROVENANCE_TIMESTAMP_END="$(date +%s)"
+
 echo "--- Build done ---"
 echo "Generating provenance..."
 
 export PROVENANCE_TIMESTAMP_BEGIN
 export PROVENANCE_TIMESTAMP_END
 export PROVENANCE_EXTERNAL_PARAMETERS
-export PROVENANCE_OUTPUT_FILE=provenance.json
 
 python main.py "$1"
