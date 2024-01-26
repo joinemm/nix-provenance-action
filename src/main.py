482271bd4e08e458269b7627806f26cb13b09cad
Joonas Rautiola <joonas.rautiola@unikie.com>
2024-01-26 15:29:27 +0200

    Add option to get dependencies recursively
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- src/main.py
+++ src/main.py
@@ -41,7 +41,7 @@ def get_subjects(outputs: dict) -> list[dict]:
             "uri": data["path"],
         }
         hash = exec_cmd(
-            ["nix-store", "-q", "--hash", data["path"]],
+            ["nix-store", "--query", "--hash", data["path"]],
             raise_on_error=False,
         )
         if hash is None:
@@ -56,12 +56,14 @@ def get_subjects(outputs: dict) -> list[dict]:
     return subjects
 
 
-def get_dependencies(drv_path: str) -> list[dict]:
+def get_dependencies(drv_path: str, recursive: bool = False) -> list[dict]:
     """Get dependencies of derivation and parse them into ResourceDescriptors"""
-    deps_drv = exec_cmd(["nix-store", "-q", "--references", drv_path]).split()
+    depth = "--requisites" if recursive else "--references"
+    deps_drv = exec_cmd(["nix-store", "--query", depth, drv_path]).split()
+
     dependencies = []
     for drv in deps_drv:
-        hash = exec_cmd(["nix-store", "-q", "--hash", drv]).strip()
+        hash = exec_cmd(["nix-store", "--query", "--hash", drv]).strip()
         hash_type, hash_value = hash.split(":")
 
         dependency_json = {
@@ -88,6 +90,22 @@ def get_dependencies(drv_path: str) -> list[dict]:
     return dependencies
 
 
+def get_external_parameters(drv_path: str) -> dict:
+    """Get externalParameters from env variable and add derivation"""
+    params = json.loads(os.environ.get("PROVENANCE_EXTERNAL_PARAMS", "{}"))
+
+    # add derivation path always to params
+    params["derivation"] = drv_path
+
+    # return only params with non-empty values
+    return {k: v for k, v in params.items() if v}
+
+
+def get_internal_parameters() -> dict:
+    """Get internalParameters from env variable"""
+    return json.loads(os.environ.get("PROVENANCE_INTERNAL_PARAMS", "{}"))
+
+
 def timestamp(unix_time: int | str | None) -> str | None:
     """Turn unix timestamp into RFC 3339 format"""
     if unix_time is None:
@@ -102,7 +120,7 @@ def timestamp(unix_time: int | str | None) -> str | None:
     )
 
 
-def provenance(target: str) -> dict:
+def provenance(target: str, recursive: bool = False) -> dict:
     """Create the provenance file"""
     drv_json = json.loads(exec_cmd(["nix", "derivation", "show", target]))
     drv_path = next(iter(drv_json))
@@ -115,13 +133,9 @@ def provenance(target: str) -> dict:
         "predicate": {
             "buildDefinition": {
                 "buildType": os.environ.get("PROVENANCE_BUILD_TYPE"),
-                "externalParameters": json.loads(
-                    os.environ.get("PROVENANCE_EXTERNAL_PARAMS", "{}")
-                ),
-                "internalParameters": json.loads(
-                    os.environ.get("PROVENANCE_INTERNAL_PARAMS", "{}")
-                ),
-                "resolvedDependencies": get_dependencies(drv_path),
+                "externalParameters": get_external_parameters(drv_path),
+                "internalParameters": get_internal_parameters(),
+                "resolvedDependencies": get_dependencies(drv_path, recursive),
             },
             "runDetails": {
                 "builder": {
@@ -145,19 +159,32 @@ def provenance(target: str) -> dict:
 
 
 def main():
-    """Main function that parses the arguments"""
+    """Main function that parses the arguments and writes provenance file"""
     parser = argparse.ArgumentParser(
         prog="nix-provenance",
         description="Get SLSA v1.0 provenance file from nix flake or derivation",
     )
-    parser.add_argument("target")
+    parser.add_argument(
+        "target",
+        help="Flake reference or derivation path",
+    )
+    parser.add_argument(
+        "--recursive",
+        action="store_true",
+        help="Resolve every dependency recursively",
+    )
+    parser.add_argument(
+        "--out",
+        help="Path to file where provenance should be saved",
+        default=os.environ.get("PROVENANCE_OUTPUT_FILE"),
+    )
     args = parser.parse_args()
 
-    schema = provenance(args.target)
+    # generate provenance
+    schema = provenance(args.target, args.recursive)
 
-    out = os.environ.get("PROVENANCE_OUTPUT_FILE")
-    if out:
-        with open(out, "w") as f:
+    if args.out:
+        with open(args.out, "w") as f:
             f.write(json.dumps(schema, indent=2))
     else:
         print(json.dumps(schema, indent=2))
