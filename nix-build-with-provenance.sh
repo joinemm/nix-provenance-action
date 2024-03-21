e05679214889675aad7489cc31fa9f34a654ba30
Joonas Rautiola <joonas.rautiola@unikie.com>
2024-03-21 15:18:26 +0200

    Source code moved to sbomnix repo

--- nix-build-with-provenance.sh
+++ nix-build-with-provenance.sh
@@ -9,13 +9,13 @@ echo "--- Starting nix build ---"
 nix build "${@}"
 
 PROVENANCE_EXTERNAL_PARAMETERS="$(jq -n --arg target "$1" --arg build-args "${2:@}" '$ARGS.named')"
-PROVENANCE_TIMESTAMP_END="$(date +%s)"
+PROVENANCE_TIMESTAMP_FINISHED="$(date +%s)"
 
 echo "--- Build done ---"
 echo "Generating provenance..."
 
 export PROVENANCE_TIMESTAMP_BEGIN
-export PROVENANCE_TIMESTAMP_END
+export PROVENANCE_TIMESTAMP_FINISHED
 export PROVENANCE_EXTERNAL_PARAMETERS
 
-python main.py "$1"
+nix run github:tiiuae/sbomnix#provenance "$1"
